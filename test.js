1acb89eb8fda80358946e1c0301aee34100811fd
Andrea Parodi <andrea@parro.it>
2017-11-20 23:16:58 +0100

    refactor

--- test.js
+++ test.js
@@ -1,60 +1,51 @@
 import test from "tape-async";
 import reduce from ".";
 
-function reducer(accum, item) {
-  return accum + item.answer;
-}
+const arr = [42, 43];
 
-function sum(accum, item) {
-  return accum + item;
-}
+const add = (accum, item) => accum + item;
 
 test("exports a function", async t => {
   t.is(typeof reduce, "function");
 });
 
 test("reduce to a sum", async t => {
-  const arr = [{ answer: 42 }, { answer: 43 }];
-  const tot = await reduce(arr, reducer, 1);
+  const tot = await reduce(arr, add, 1);
   t.is(tot, 86);
 });
 
 test("the initial accumulator value default to the first item", async t => {
-  const arr = [42, 43];
-  const tot = await reduce(arr, sum);
+  const tot = await reduce(arr, add);
   t.is(tot, 85);
 });
 
 test("can be partially applied", async t => {
-  const sum = reduce.with(reducer, 1);
-  const tot = await sum([{ answer: 42 }, { answer: 43 }]);
+  const sum = reduce.with(add, 1);
+  const tot = await sum(arr);
   t.is(tot, 86);
 });
 
 test("initial accumulator value default to the first item when partially applied", async t => {
-  const reduceSum = reduce.with(sum);
-  const tot = await reduceSum([42, 43]);
+  const reduceSum = reduce.with(add);
+  const tot = await reduceSum(arr);
   t.is(tot, 85);
 });
 
 test("reducer receive item, index, iterable", async t => {
-  const arr = [{ answer: 42 }, { answer: 43 }];
   const result = [];
   await reduce(
     arr,
     (accumulator, item, index, iterable) => {
-      result.push({ index });
+      result.push(index);
       t.is(iterable, arr);
-      return true;
     },
     0
   );
 
-  t.deepEqual(result, [{ index: 0 }, { index: 1 }]);
+  t.deepEqual(result, [0, 1]);
 });
 
 test("first index is 1 when initial accumulator not provided", async t => {
-  const arr = [42, 43];
   const result = [];
   await reduce(arr, (accumulator, item, index, iterable) => {
     result.push(index);
@@ -67,30 +58,22 @@ test("first index is 1 when initial accumulator not provided", async t => {
 });
 
 test("predicate could return a promise", async t => {
-  const arr = [{ answer: 42 }, { answer: 43 }];
-  const tot = await reduce(
-    arr,
-    async (accum, item) => {
-      return accum + item.answer;
-    },
-    0
-  );
+  const tot = await reduce(arr, (accum, item) => Promise.resolve(accum + item));
   t.is(tot, 85);
 });
 
 test("throw async if data is not an async iterable", async t => {
-  const err = await reduce(0, () => 0, 0).catch(err => err);
+  const err = await reduce(0, () => 0).catch(err => err);
 
   t.is(err.message, "data argument must be an iterable or async-iterable.");
 });
 
-test("throw async if data is reducer is not a function", async t => {
-  const err = await reduce(0, 0, 0).catch(err => err);
-
+test("throw async if reducer is not a function", async t => {
+  const err = await reduce([0], 0).catch(err => err);
   t.is(err.message, "reducer argument must be a function.");
 });
 
-test("throw async during iteration if reducer throws", async t => {
+test("throw async if reducer throws", async t => {
   const err = await reduce(
     ["ciao"],
     () => {
@@ -102,7 +85,7 @@ test("throw async during iteration if reducer throws", async t => {
   t.is(err.message, "test");
 });
 
-test("throw async during iteration if predicate rejected", async t => {
+test("throw async during iteration if reducer return a rejected promise", async t => {
   const err = await reduce(
     ["ciao"],
     async () => {
